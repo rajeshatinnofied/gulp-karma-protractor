8c6aa16378fc7f37045005504c285348a2ce1f43
rajeshatinnofied <rajesh.dan@innofied.com>
2015-09-18 17:27:09 +0530

    Fixed unit test issue

--- src/app/user/user.controller.spec.js
+++ src/app/user/user.controller.spec.js
@@ -2,18 +2,7 @@
     'use strict';
 
     describe('User-controller', function() {
-        var $state, userId, $controller, vm;
         beforeEach(module('newtestproject'));
-        // beforeEach(inject(function(_$state_) {
-        //     $state = _$state_;
-        //     userId = {
-        //         userId: 'admin'
-        //     };
-        //     vm = $controller('UserController', {
-        //         '$state': $state,
-        //         'userId': userId
-        //     });
-        // }));
 
         it('should define user name', inject(function($controller) {
             var userId = 'admin';
